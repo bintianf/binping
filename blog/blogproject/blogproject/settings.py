ee17d5cab1441d5cf972db25362134113bf49019
bintianf <bintianf@gmail.com>
2018-12-28 11:33:54 -0500

    update1

--- blog/blogproject/blogproject/settings.py
+++ blog/blogproject/blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uu05m3cd5jl0a=kb*th)+l(hy^@byb3187yprt(bw^!*jwg2%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.binping.tech']
 
 
 # Application definition
@@ -124,7 +124,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # 提供搜索功能
 HAYSTACK_CONNECTIONS = {
     'default': {
