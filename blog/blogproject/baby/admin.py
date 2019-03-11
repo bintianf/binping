68a519280eff44b89cd64895d593c927fb952880
bintianf <bintianf@gmail.com>
2019-03-11 11:42:34 -0400

    2

--- blog/blogproject/baby/admin.py
+++ blog/blogproject/baby/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Picture, Age, Group
+# from .models import Picture, Age, Group
 
 # class PicAdmin(admin.ModelAdmin):
 #     list_display = ['picurl', 'title', 'created_time', 'modified_time', 'age', 'group']
